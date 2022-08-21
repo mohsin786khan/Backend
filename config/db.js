157b0369da5cbde6df89982a915d13dbeacbaf8f
mohsin khan <khan.mohsin.00001@gmail.com>
2022-08-22 00:23:06 +0530

    connected with database

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
-// const mongoose = require('mongoose');
+ const mongoose = require('mongoose');
 
-// const connectDB = () => {
+const connectDB = async () => {
     
-// }
+    const con = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB connected: ${con.connection.host}`);
+
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
 
 
 // set config path to load env variables
 dotenv.config({ path: './config/config.env' });
 
+// connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 
 const app = express();
 
-const logger = (req, res, next) => {
-  req.hello = 'Hellow World';
-  console.log('middleware run');
-  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalURL}`)
-  next();
-};
-
-app.use(logger);
-
-// Dev logging middleware
-//if (process.env.NODE_ENV === 'development') {
-  //  app.use(morgan('dev'));
-  //}
+ // Dev logging middleware
+ if (process.env.NODE_ENV === 'development') {
+   app.use(morgan('dev'));
+  }
 
 
 // Mount routers
@@ -35,5 +30,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,
+  const server = app.listen(PORT,
     console.log(`server is running in ${process.env.NODE_ENV} mode on ${PORT} port`) );
+
+
+  // Handle unhandled promise rejections
+  process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+   // Close server & exit process
+  server.close(() => process.exit(1));
+  });
+
